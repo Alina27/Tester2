41051ef7b81d18a8f156b5e91292156ca96c4e2f
alina <alinashevchuk27@gmail.com>
2019-05-29 12:32:07 +0300

    Update2 to HelloWorldFourth

--- src/main/scala/HelloWorldFourth.scala
+++ src/main/scala/HelloWorldFourth.scala
@@ -2,5 +2,6 @@ object HelloWorldFourth {
   def main(args: Array[String]): Unit = {
     println("Hello, from HelloWorldFourth !")
     println("HelloWorldFourth! Update 1")
+    println("HelloWorldFourth! Update 2")
   }
 }
